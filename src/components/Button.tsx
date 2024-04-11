21c2a8a719af7df4ca9bf64bb34fca5befbcd2b5
Alysson Bormannn <alysson.bormannn@gmail.com>
2024-04-11 17:36:54 -0400

    create layout default

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonVariant, ButttonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButttonContainer variant={variant}>Enviar</ButttonContainer>
-}
