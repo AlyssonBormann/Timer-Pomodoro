b23cc80b61e40e1d15a5f6c077350409adba731b
Alysson Bormannn <alysson.bormannn@gmail.com>
2024-04-30 18:12:20 -0400

    finalizando IGNITE-TIMER

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useReducer, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+import { ReactNode, createContext, useEffect, useReducer, useState } from 'react'
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
@@ -31,16 +32,34 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cylesState, dispatch] = useReducer(cyclesReducer, {
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null,
+  }, (initialState)=>{
+    const storageStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0')
+    
+    if(storageStateAsJSON){
+      return JSON.parse(storageStateAsJSON)
+    }
+
+    return initialState
   })
 
-  const { cycles, activeCycleId } = cylesState
-
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
+  const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(()=>{
+   if(activeCycle){
+    return differenceInSeconds(
+      new Date(),
+      new Date(activeCycle.startDate),
+    )
+   }
+
+   return 0
+  })
+
+  
 
   function markCurrentCycleAsFinished() {
     dispatch(markCurrentCyclesAsFinishedAction())
@@ -70,6 +89,12 @@ export function CyclesContextProvider({
 
     document.title = `Ignite Timer`
   }
+
+  useEffect(()=>{
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  },[cyclesState])
 
   return (
     <CyclesContext.Provider
